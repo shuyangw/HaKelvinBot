3309bdec429bc16f35a5d1b68e8e62f49d249e77
Shuyang <qwqw1337@gmail.co>
2020-07-24 20:45:33 -0400

    Initial commit of project files

---
+++ HaKelvinBot/HaKelvinBot/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HaKelvinBot
+{
+    public class Program
+    {
+        private const string KEY = "NzM2MzgwNDU2OTc4ODc0Mzc4.Xxt9vg.tKtBc0g-qbVqqGJYFC7py12gQts";
+
+        public static void Main(string[] args) =>
+            new Program().MainAsyncTask().GetAwaiter().GetResult();
+
+        public async Task MainAsyncTask()
+        {
+
+        }
+    }
+}
