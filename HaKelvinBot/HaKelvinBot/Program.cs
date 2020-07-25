77205453f24b2a56e2aec165246f7e36667a0bfe
Shuyang <qwqw1337@gmail.co>
2020-07-24 21:53:55 -0400

    Complete echo functionality

--- HaKelvinBot/HaKelvinBot/Program.cs
+++ HaKelvinBot/HaKelvinBot/Program.cs
@@ -10,17 +10,22 @@ namespace HaKelvinBot
     {
         private const string KEY = "NzM2MzgwNDU2OTc4ODc0Mzc4.Xxt9vg.Qz7wNxOtIORvGubqR7ZSZYh7Suo";
 
-        private DiscordSocketClient _client;
+        private const ulong MAIN_CHANNEL_ID = 158732651010850816;
+
+        private DiscordSocketClient client_;
 
         public static void Main(string[] args) =>
             new Program().MainAsyncTask().GetAwaiter().GetResult();
 
         public async Task MainAsyncTask()
         {
-            _client = new DiscordSocketClient();
+            Console.WriteLine("Connecting!");
 
-            _client.Log += Log;
+            client_ = new DiscordSocketClient();
 
+            client_.Log += Log;
+            client_.MessageReceived += Message_Received;
+            
             //  You can assign your bot token to a string, and pass that in to connect.
             //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
             var token = KEY;
@@ -30,11 +35,23 @@ namespace HaKelvinBot
             // var token = File.ReadAllText("token.txt");
             // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
 
-            await _client.LoginAsync(TokenType.Bot, token);
-            await _client.StartAsync();
+            await client_.LoginAsync(TokenType.Bot, token);
+            await client_.StartAsync();
 
             // Block this task until the program is closed.
             await Task.Delay(-1);
+        }
+
+
+        private Task Message_Received(SocketMessage arg)
+        {
+            ulong targetChannelId = arg.Channel.Id;
+            if (arg.Author.Username.Contains("Dank Memes"))
+                (client_.GetChannel(targetChannelId) as IMessageChannel).SendMessageAsync("Shut up Kelvin");
+            else if (arg.Author.Username.Contains("ShwangCat"))
+                (client_.GetChannel(targetChannelId) as IMessageChannel).SendMessageAsync("Howdy partner");
+
+            return Task.CompletedTask;
         }
 
         private Task Log(LogMessage msg)
@@ -42,5 +59,7 @@ namespace HaKelvinBot
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
         }
+
+
     }
 }
