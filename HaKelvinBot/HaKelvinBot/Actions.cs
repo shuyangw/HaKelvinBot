0387f3a7f79dcf74f38d7c44966554a5021637e7
Shuyang <qwqw1337@gmail.co>
2020-07-29 00:22:57 -0400

    Add SendMessage overload

--- HaKelvinBot/HaKelvinBot/Actions.cs
+++ HaKelvinBot/HaKelvinBot/Actions.cs
@@ -1,4 +1,5 @@
 ﻿using Discord;
+using Discord.WebSocket;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -14,6 +15,30 @@ namespace HaKelvinBot
             Debug.Assert(client_ != null);
 
             (client_.GetChannel(targetChannelId) as IMessageChannel).SendMessageAsync(message);
+            return Task.CompletedTask;
+        }
+
+        public Task SendMessage(string channelName, string message)
+        {
+            int countOfSameNameChannels = 0;
+            ulong desiredChannelId = 0;
+            foreach (var server in client_.Guilds)
+            {
+                foreach (var channel in server.TextChannels)
+                {
+                    if (channel.Name.ToLowerInvariant() == channelName.ToLowerInvariant())
+                    {
+                        ++countOfSameNameChannels;
+                        desiredChannelId = channel.Id;
+                    }
+                }
+            }
+            if (countOfSameNameChannels != 1)
+                return Task.CompletedTask;
+
+            Debug.Assert(client_ != null);
+
+            SendMessage(desiredChannelId, message);
             return Task.CompletedTask;
         }
     }
