e5b8162262e84012f2fc01087c2a3e4f37b85662
Shuyang <qwqw1337@gmail.co>
2020-07-30 02:14:33 -0400

    Re-add non-ignored files

---
+++ HaKelvinBot/EventListeners.cs
@@ -0,0 +1,71 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaKelvinBot
+{
+    partial class Program
+    {
+        private const string ADMIN_SERVER_NAME = "BotTesting";
+
+        private Task Message_Received(SocketMessage arg)
+        {
+            if (((arg.Channel) as SocketGuildChannel).Guild.Name == ADMIN_SERVER_NAME && arg.Content[0] == '!')
+            { 
+                //Remove exclamation mark
+                ParseAdminMessage(arg.Content.Replace("!", ""));
+                goto Finish;
+            }
+
+            ulong targetChannelId = arg.Channel.Id;
+            if (arg.Author.Username.Contains(MainUserKelvin.Username))
+            {
+                if (Allow("EchoTask_KelvinEcho"))
+                    SendMessage(targetChannelId, "Shut up Kelvin");
+            }
+            else if (arg.Author.Username.Contains(MainUserShwang.Username))
+            {
+                if (Allow("EchoTask_ShwangEcho"))
+                    SendMessage(targetChannelId, "Howdy partner");
+            }
+
+            Finish:
+                return Task.CompletedTask;
+        }
+
+        private void ParseAdminMessage(string message)
+        {
+            switch (message)
+            {
+
+                default:
+                    break;
+            }
+        }
+
+        private bool Allow(string taskName)
+        {
+            return FloodValid(taskName) && AllowedResponses[taskName];
+        }
+
+        private bool FloodValid(string taskName)
+        {
+            Debug.Assert(FloodPreventionInfo != null); 
+
+            if (!FloodPreventionInfo.ContainsKey(taskName))
+                return false;
+
+            long prevTime = FloodPreventionInfo[taskName].Item1;
+            long currTime = GetUnixTime();
+            bool val = (currTime - prevTime >= FloodPreventionInfo[taskName].Item2);
+            if (val)
+                FloodPreventionInfo[taskName] = Tuple.Create(currTime, FloodPreventionInfo[taskName].Item2);
+           
+            return val;
+        }
+    }
+}
---
+++ HaKelvinBot/Structures/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HaKelvinBot.Structures
+{
+    public class User
+    {
+        #region Properties
+        public string Nicknane { get; set; }
+
+        public string Username { get; set; }
+        #endregion
+    }
+}
