fbf4512e46114fc740d7e1126cc764a1104392f5
Shuyang <qwqw1337@gmail.co>
2020-07-30 02:05:17 -0400

    Restructure project file structure

---
+++ HaKelvinBot/Util/Configuration.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using YamlDotNet.RepresentationModel;
+
+namespace HaKelvinBot.Util
+{
+    public class Configuration
+    {
+        private static Dictionary<string, string> Values { get; set; }
+
+        public static void Load(string fileName)
+        {
+            Console.WriteLine("Ingesting config file...");
+            string wantedPath = AppContext.BaseDirectory;
+            if (!File.Exists(Path.Combine(wantedPath, fileName)))
+                return;
+            Values = new Dictionary<string, string>();
+
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                var yaml = new YamlStream();
+                yaml.Load(sr);
+
+                var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
+                var initMapping = (YamlMappingNode)mapping.Children[new YamlScalarNode("init")];
+
+                foreach(var item in initMapping)
+                    Values.Add(item.Key.ToString().ToLowerInvariant(), item.Value.ToString());
+            }
+            Console.WriteLine("Successfully loaded config file!");
+        }
+
+        public static string Get(string key)
+        {
+            key = key.ToLowerInvariant();
+            if (Values.ContainsKey(key))
+                return Values[key];
+            else
+                return null;
+        }
+    }
+}
